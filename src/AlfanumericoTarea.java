a0cc213cec796d907fbce8c18e2f26d84d75f6cc
Franco Romeo <francoromeo011@hotmail.com>
2023-01-21 15:11:19 -0300

    Initial commit

---
+++ src/AlfanumericoTarea.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.TimeUnit;
+
+public class AlfanumericoTarea implements Runnable{
+
+    private Tipo tipo;
+
+    public AlfanumericoTarea(Tipo tipo){
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void run() {
+        try{
+            switch (tipo) {
+                case NUMERO -> {
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println(i);
+                        TimeUnit.SECONDS.sleep(1);
+                    }
+                }
+                case LETRA -> {
+                    for (char c = 'A'; c <= 'Z'; c++) {
+                        System.out.println(c);
+                        TimeUnit.SECONDS.sleep(1);
+                    }
+                }
+            }
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+}
